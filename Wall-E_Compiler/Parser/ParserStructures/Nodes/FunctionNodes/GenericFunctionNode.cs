fa7057cff0f601c8fecc47b538c750775b2d8e54
Tuto <tuto210705@gmail.com>
2025-06-07 23:29:46 -0400

    Minimal changes at generic function structure.

--- Wall-E_Compiler/Parser/ParserStructures/Nodes/FunctionNodes/GenericFunctionNode.cs
+++ Wall-E_Compiler/Parser/ParserStructures/Nodes/FunctionNodes/GenericFunctionNode.cs
@@ -4,9 +4,39 @@ namespace Parser
 {
     abstract class GenericFunction(string lex, int line, FunctionType functionType, List<GenericNode> arguments) : GenericNode(lex, line)
     {
+
+
+        public static readonly Dictionary<FunctionType, int> ArgsCount = new Dictionary<FunctionType, int>()
+        {
+            { FunctionType.Spawn,2},
+            { FunctionType.Color,1},
+            { FunctionType.Size,1},
+            { FunctionType.DrawLine,3},
+            { FunctionType.DrawCircle,3},
+            { FunctionType.DrawRectangle,5},
+            { FunctionType.Fill,0},
+            { FunctionType.GetActualX,0},
+            { FunctionType.GetActualY,0},
+            { FunctionType.GetCanvasSize,0},
+            { FunctionType.GetColorCount,5},
+            { FunctionType.IsBrushColor,1},
+            { FunctionType.IsCanvasColor,3},
+            { FunctionType.IsBrushSize,1}
+        };
         public FunctionType FunctionType { get; private set; } = functionType;
         public List<GenericNode> Arguments { get; private set; } = arguments;
-        public abstract void ValidateArgument();
+        public void ValidateArgument()
+        {
+            if (!(Arguments.Count == ArgsCount[FunctionType]))
+            {
+                _ = new Exception(ExceptionType.Argument, Line, Lex);
+                return;
+            }
+            SpecialValidation();
+        }
+        protected abstract void SpecialValidation();
+
+
     }
     public enum FunctionType
     {
